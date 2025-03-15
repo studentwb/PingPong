c09d6c08251954729eedc50c099a5d8697b20fb1
Wojciech Baron <wabaron98@gmail.com>
2025-03-15 15:42:32 +0100

    Add first entity

--- Server.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Server.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Server.Domain/Entites/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Domain.Entites
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Result { get; set; }
+        public virtual int PlayerId { get; set; }
+    }
+}
