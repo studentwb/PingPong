81f9c67b925cf4e9d794784f7e96cfd51269dbad
Wojciech Baron <wabaron98@gmail.com>
2025-03-24 10:12:03 +0100

    cleanup code

--- pingpong.client/src/components/Ball.vue
+++ pingpong.client/src/components/Ball.vue
@@ -73,7 +73,7 @@
   .ball {
     width: 50px;
     height: 50px;
-    background-image: url("/src/components/icons/ball.png");
+    background-image: url("/src/components/icons/ball.jpg");
     background-size: contain;
     border-radius: 50%;
     position: absolute;
